4cd71039540506e36112e7e76a69dfbdb22d79df
mahendra <mahendrar23@gmail.com>
2017-02-21 23:16:25 +0700

    renaming file

---
+++ scripts/index.R
@@ -0,0 +1,3 @@
+input.offers = read.csv("../dataset/offers.csv")
+input.history_train = read.csv("../dataset/trainHistory.csv")
+input.transactions = read.csv("../dataset/transactions.csv", nrows=5)
